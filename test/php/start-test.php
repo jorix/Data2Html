83b566063f50693162cb90b5c89375e56d5a0e04
jorix <xavier.mamano@gmail.com>
2018-05-09 20:02:56 +0200

    primeres probres

---
+++ test/php/start-test.php
@@ -0,0 +1,5 @@
+<?php
+// TODO: a PSR-4 loader ->  https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-4-autoloader-examples.md
+    require_once "../../code/php/Autoload.php";
+    Data2Html_Autoload::start(__DIR__, 'd2h_config.ini');
+    
---
+++ test/php/tests/unit/ControllerValidateTest.php
@@ -0,0 +1,26 @@
+<?php
+
+class ControllerValidateTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+        require 'start-test.php';
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+        $val = new Data2Html_Controller_Validate('dummy');
+        $this->assertEquals('bill', $val->validateValue('x', ['required' => true]));
+        
+    }
+}
